874cefe3cb5b0c670ecc4ee8255628e2e0696ae1
heisenbuggs <prasukjain0047@gmail.com>
2024-05-31 13:19:35 +0530

    StackOverflow Problem

---
+++ StackOverflow/src/main/java/Comment.java
@@ -0,0 +1,8 @@
+import java.security.Timestamp;
+
+public class Comment {
+    private int id;
+    private String text;
+    private User author;
+    private Timestamp createdAt;
+}
---
+++ StackOverflow/src/main/java/StackOverflowMain.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StackOverflowMain {
+    public static void main(String[] args) {
+        StackOverflow stackOverflow = StackOverflow.getInstance();
+
+        // Register users
+        User user1 = new User(1, "Prasuk", "password123", "prasuk@example.com", 0);
+        User user2 = new User(2, "Nimish", "password456", "nimish@example.com", 0);
+        stackOverflow.registerUser(user1);
+        stackOverflow.registerUser(user2);
+
+        // User login
+        User loggedInUser = stackOverflow.loginUser("Prasuk", "password123");
+        if (loggedInUser != null) {
+            System.out.println("User logged in: " + loggedInUser.getUsername());
+        } else {
+            System.out.println("Invalid username or password.");
+        }
+
+        // Post a question
+        Question question1 = new Question(1, "What is Java?", "Please explain Java.", user1, new ArrayList<>(),
+                new ArrayList<>(), Arrays.asList(new Tag(1, "java"), new Tag(2, "programming")), 0);
+        stackOverflow.postQuestion(question1);
+
+        // Post an answer
+        Answer answer1 = new Answer(1, "Java is an object-oriented programming language.", user2, question1,
+                new ArrayList<>(), 0);
+        stackOverflow.postAnswer(answer1);
+
+        // Vote on the question
+        stackOverflow.voteQuestion(question1, 1);
+
+        // Search for questions
+        List<Question> searchResults = stackOverflow.searchQuestions("Java");
+        System.out.println("Search Results:");
+        for (Question question : searchResults) {
+            System.out.println(question.getTitle());
+        }
+
+        // Get questions by tag
+        List<Question> taggedQuestions = stackOverflow.getQuestionsByTag("java");
+        System.out.println("Questions tagged with 'java':");
+        for (Question question : taggedQuestions) {
+            System.out.println(question.getTitle());
+        }
+
+        // Get questions by user
+        List<Question> userQuestions = stackOverflow.getQuestionsByUser(user1);
+        System.out.println("Questions posted by user1:");
+        for (Question question : userQuestions) {
+            System.out.println(question.getTitle());
+        }
+    }
+}

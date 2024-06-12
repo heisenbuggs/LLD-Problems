68399f0d4f21cb01fd74379ba032c97c68dab555
heisenbuggs <prasukjain0047@gmail.com>
2024-06-12 08:04:00 +0530

    LinkedIn

---
+++ LinkedIn/src/main/java/Connection.java
@@ -0,0 +1,19 @@
+import java.sql.Timestamp;
+
+public class Connection {
+    private final User user;
+    private final Timestamp connectionDate;
+
+    public Connection(User user, Timestamp connectionDate) {
+        this.user = user;
+        this.connectionDate = connectionDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Timestamp getConnectionDate() {
+        return connectionDate;
+    }
+}
---
+++ LinkedIn/src/main/java/NotificationType.java
@@ -0,0 +1,5 @@
+public enum NotificationType {
+    CONNECTION_REQUEST,
+    MESSAGE,
+    JOB_POSTING
+}

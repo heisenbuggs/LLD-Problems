f9ba622ee2eff10420d59d49c023d2cb0498a5db
heisenbuggs <prasukjain0047@gmail.com>
2024-06-24 08:59:48 +0530

    Restaurant Management System

---
+++ RestaurantManagementSystem/src/main/java/OrderStatus.java
@@ -0,0 +1,7 @@
+public enum OrderStatus {
+    PENDING,
+    PREPARING,
+    READY,
+    COMPLETED,
+    CANCELLED
+}
---
+++ RestaurantManagementSystem/src/main/java/payment/PaymentMethod.java
@@ -0,0 +1,7 @@
+package payment;
+
+public enum PaymentMethod {
+    CASH,
+    CREDIT_CARD,
+    MOBILE_PAYMENT
+}

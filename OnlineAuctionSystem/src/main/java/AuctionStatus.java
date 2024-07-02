3bad5bcbba5e08139f0e3f410a6ea23c49b9d484
heisenbuggs <prasukjain0047@gmail.com>
2024-07-02 09:25:22 +0530

    Online Auction System

---
+++ OnlineAuctionSystem/src/main/java/AuctionStatus.java
@@ -0,0 +1,4 @@
+public enum AuctionStatus {
+    ACTIVE,
+    CLOSED
+}
---
+++ OnlineAuctionSystem/src/main/java/User.java
@@ -0,0 +1,17 @@
+public class User {
+    private final String id;
+    private final String username;
+    private final String email;
+
+    public User(String id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+}
